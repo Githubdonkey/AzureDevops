8a49e2c9dfe77af75bd48821a398714e723c15fc
Kaine Eckler <ecklerkaine@gmail.com>
2020-01-19 01:47:10 -0500

    01-19-20-01-47

--- build_menu.sh
+++ build_menu.sh
@@ -34,35 +34,33 @@ case $n in
   11) echo "You chose Option 11" 
      cd packer/
      packer build aws_ubuntu18_latest_marketplace.json;;
+
   12) echo "You chose Option 12" 
      cd packer/
      packer build aws_win2016_latest_marketplace.json
      export TF_VAR_packer_image=$(cat packer/manifest.json | jq '.builds | to_entries[] | .value.artifact_id' | tr -d '"')
      aws s3 cp packer/manifest.json s3://gitdonkey/devops/${TF_VAR_packer_image}.json;;
-  13)echo "AWS check for latest Packer Image"
-     if [ "$(cat packer/manifest.json | jq '.builds | to_entries[] | .value.artifact_id' | tr -d '"'| cut -d':' -f2)" ]
-      then
+
+  13) string=$(cat packer/manifest.json | jq '.builds | to_entries[] | .value.name')
+     if [[ $string == *"amazon"* ]]; then
         echo "AWS is NOT empty"
-     elif [ $(cat packer/manifest.json | jq '.builds | to_entries[] | .value.custom_data.managed_image_name' | tr -d '"') ]
-      then
+        export TF_VAR_packer_image=$(cat packer/manifest.json | jq '.builds | to_entries[] | .value.artifact_id' | tr -d '"' | cut -d':' -f2)
+        cp terraform/aws_main.tf dev/main.tf
+     elif [[ $string == *"azure-arm"* ]]; then
         echo "Azure is NOT empty"
+        export TF_VAR_packer_image=$(cat packer/manifest.json | jq '.builds | to_entries[] | .value.artifact_id' | tr -d '"')
+        #terraform plan;;
      else
         echo "packer/manifest.json not found"
      fi
-     
-     
-     if [ -z "$(cat packer/manifest.json | jq '.builds | to_entries[] | .value.artifact_id' | tr -d '"'| cut -d':' -f2)" ]
-      then
-      echo "$(tput setaf 1) \$TF_VAR_packer_image is empty$(tput sgr 0)"
-        else
-      echo "\$TF_VAR_packer_image is NOT empty"
-        fi
-     export TF_VAR_packer_image=$(cat packer/manifest.json | jq '.builds | to_entries[] | .value.artifact_id' | tr -d '"'| cut -d':' -f2)
-     export TF_VAR_packer_image1=$(cat packer/manifest.json | jq '.builds | to_entries[] | .value.custom_data.managed_image_name' | tr -d '"')
      echo $TF_VAR_packer_image
-     echo $TF_VAR_packer_image1;;
-     #terraform plan;;
-     #terraform apply -auto-approve;;
+     cd dev/
+     terraform init
+     terraform plan -var="image_id=$TF_VAR_packer_image"
+     terraform apply -var="image_id=$TF_VAR_packer_image" -auto-approve
+     echo "slleep 4m"
+     sleep 4m
+     terraform destroy -var="image_id=$TF_VAR_packer_image" -auto-approve;;
   21) echo "You chose Option 21" 
      cd packer/
      packer build azure_ubuntu18_latest_marketplace.json;;
--- dev/linuxUbuntuMicro.tfvars
+++
@@ -1,4 +0,0 @@
-createInstanceLinux     = false
-createInstanceWin       = false
-createS3_public         = false
-createS3_private        = false
