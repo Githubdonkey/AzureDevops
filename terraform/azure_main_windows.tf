63357877713d6099992a2bbfdf1005f9bcf3b328
Kaine Eckler <ecklerkaine@gmail.com>
2020-04-25 16:40:35 -0400

    04-25-20-16-40

--- terraform/azure_main_windows.tf
+++ terraform/azure_main_windows.tf
@@ -3,17 +3,31 @@ provider "azurerm" {
   features {}
 }
 
-variable "image_id" {}
+variable "image_id" {
+    description = "Image name"
+    default = ""
+}
+
+variable "location" {
+    description = "location of image"
+    default = "eastus"
+}
+
+#declare local
+locals {
+  timestamp = "${timestamp()}"
+  timestamp_sanitized = "${replace("${local.timestamp}", "/[- TZ:]/", "")}"
+}
 
 resource "azurerm_resource_group" "rg" {
-  name = "my-first-terraform-rg"
-  location = "eastus"
+  name = "rg-${local.timestamp_sanitized}"
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "myvnet" {
-  name = "my-vnet"
+  name = "vnet-${local.timestamp_sanitized}"
   address_space = ["10.0.0.0/16"]
-  location = "eastus"
+  location = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
@@ -26,19 +40,19 @@ resource "azurerm_subnet" "frontendsubnet" {
 
 resource "azurerm_public_ip" "myvm1publicip" {
   name = "pip1"
-  location = "eastus"
+  location = var.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method = "Dynamic"
   sku = "Basic"
 }
 
 resource "azurerm_network_interface" "myvm1nic" {
-  name = "myvm1-nic"
-  location = "eastus"
+  name = "nic-${local.timestamp_sanitized}"
+  location = var.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name = "ipconfig1"
+    name = "ipconfig-${local.timestamp_sanitized}"
     subnet_id = azurerm_subnet.frontendsubnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = azurerm_public_ip.myvm1publicip.id
@@ -46,11 +60,11 @@ resource "azurerm_network_interface" "myvm1nic" {
 }
 
 resource "azurerm_windows_virtual_machine" "example" {
-  name                  = "myvm1"  
-  location              = "eastus"
+  name                  = "t${local.timestamp_sanitized}"  
+  location              = var.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.myvm1nic.id]
-  size                  = "Standard_B1s"
+  size                  = "Standard_F8s_v2"
   admin_username        = "adminuser"
   admin_password        = "Password123!"
 
