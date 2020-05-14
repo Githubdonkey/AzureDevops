a64aa6f5b043a321b5af1c1299516e4a395421f9
Kaine Eckler <ecklerkaine@gmail.com>
2020-05-14 19:02:57 -0400

    05-14-20-19-02

---
+++ packer/scripts/Installers/updateAwsCreds.ps1
@@ -0,0 +1,14 @@
+﻿
+if (Test-Path "$env:USERPROFILE\.aws\config") {Remove-Item "$env:USERPROFILE\.aws\config"}
+#Add-Content -Path "$env:USERPROFILE\.aws\config" -Value '[default]'
+#Add-Content -Path "$env:USERPROFILE\.aws\config" -Value "region = $env:USERNAME"
+#Add-Content -Path "$env:USERPROFILE\.aws\config" -Value 'output=json'
+
+if (Test-Path "$env:USERPROFILE\.aws\credentials") {Remove-Item "$env:USERPROFILE\.aws\credentials"}
+#Add-Content -Path "$env:USERPROFILE\.aws\credentials" -Value '[default]'
+#Add-Content -Path "$env:USERPROFILE\.aws\credentials" -Value "aws_access_key_id = $env:USERNAME"
+#Add-Content -Path "$env:USERPROFILE\.aws\credentials" -Value "aws_secret_access_key = $env:USERNAME"
+
+aws configure set aws_access_key_id $env:USERNAME
+aws configure set aws_secret_access_key $env:USERNAME
+aws configure set default.region $env:USERNAME
